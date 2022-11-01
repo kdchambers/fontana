5e8e1413e41374228b59926eea29bd1934e1ac3c
kdchambers <kd.chambers97@gmail.com>
2022-11-01 17:16:01 -0500

    misc: Expose API through fontana.zig

--- src/fontana.zig
+++ src/fontana.zig
@@ -1,3 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2022 Keith Chambers
 
+pub const otf = @import("otf.zig");
+pub const rasterizer = @import("rasterizer.zig");
+pub const geometry = @import("geometry.zig");
+pub const graphics = @import("graphics.zig");
