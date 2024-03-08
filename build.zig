2410c831785604fa4292c638abc884c4cdc8ef4a
kdchambers <kd.chambers97@gmail.com>
2024-03-07 21:02:30 -0500

    build: add fontana module so it can be imported via zig.zon

--- build.zig
+++ build.zig
@@ -16,6 +16,10 @@ pub fn build(b: *Build) void {
     const run_test_cmd = b.addRunArtifact(unit_tests);
     run_test_cmd.step.dependOn(b.getInstallStep());
 
+    _ = b.addModule("fontana", .{
+        .root_source_file = .{ .path = "src/fontana.zig" },
+    });
+
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_test_cmd.step);
 }
