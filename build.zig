46bb85c98efbca72801c441aaaa7a4ae0973719f
Keith Chambers <keithchambers@protonmail.com>
2024-07-30 20:18:37 -0500

    update to zig version 0.14.0-dev.716+38e0f049c

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *Build) void {
 
     const unit_tests = b.addTest(.{
         .name = "Unit Tests",
-        .root_source_file = .{ .path = "src/fontana.zig" },
+        .root_source_file = .{ .src_path = .{ .sub_path = "src/fontana.zig", .owner = b } },
         .target = target,
         .optimize = optimize,
     });
@@ -17,7 +17,7 @@ pub fn build(b: *Build) void {
     run_test_cmd.step.dependOn(b.getInstallStep());
 
     _ = b.addModule("fontana", .{
-        .root_source_file = .{ .path = "src/fontana.zig" },
+        .root_source_file = .{ .src_path = .{ .sub_path = "src/fontana.zig", .owner = b } },
     });
 
     const test_step = b.step("test", "Run library tests");
